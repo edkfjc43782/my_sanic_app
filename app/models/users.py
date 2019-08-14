65fdeb754c9cba34db5cbbafbd86c9c5985227fc
unknown <lienchewei5@gmail.com>
2019-08-14 17:07:52 +0800

    Test find a user by username

--- app/models/users.py
+++ app/models/users.py
@@ -8,3 +8,15 @@ class User(BaseModel):
     id = AutoField()
     username = CharField(null=False)
     password = CharField(null=False)
+
+    @classmethod
+    def create_user(cls, username, password):
+        if not username or not password:
+            raise ValueError
+        user = cls.create(username=username, password=password)
+        return user
+
+    @classmethod
+    def find_by_username(cls, username):
+        user = cls.get_or_none(cls.username == username)
+        return user
--- app/tests/test_users.py
+++ app/tests/test_users.py
@@ -1,6 +1,10 @@
 import pytest
 
 from models.users import User
+from models.create_db import create_tables
+
+
+create_tables()
 
 
 async def test_create_user_with_username_password():
@@ -9,16 +13,24 @@ async def test_create_user_with_username_password():
     """
     username = "qaz123"
     password = "1qaz2wsx"
-    user = User(username=username, password=password)
+    user = User.create_user(username=username, password=password)
     assert user.username == username
     assert user.password == password
 
 
-async def test_new_user_invalid_username():
+async def test_create_valid_new_user():
     """
-        Test creating a new user with invalid username
+        Test creating a new user with invalid value
     """
     with pytest.raises(ValueError):
-        user = User(username=None, password="1qaz2wsx")
-        if not user.username:
-            raise ValueError
+        User.create_user(username=None, password="1qaz2wsx")
+
+
+async def test_find_user_by_username():
+    """
+    Test find a user by username
+    """
+    username = "qaz123"
+    user = User.find_by_username(username)
+    if user:
+        assert user.username == username
